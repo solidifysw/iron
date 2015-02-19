739299c0b2306d8746ba1d535a5c0b2978945dc3
JR <jonathan.robins@solidifyhr.com>
2015-02-19 11:33:11 -0600

    Updated

--- dbUtilities/coverage query.sql
+++ dbUtilities/coverage query.sql
@@ -1,5 +1,5 @@
 select FE.Coverages.coverageId, FE.Coverages.benefit, FE.People.firstName, FE.People.lastName,FE.People.ssn, FE.People.gender, FE.People.dateOfBirth, FE.Products.solidifyId, 
-FE.ElectionTypes.name AS enrolled, IFNULL(FE.DependentsToEmployees.relationship, 'EMPLOYEE') AS relationship, FE.Coverages.pending, FE.Coverages.declineReason
+FE.ElectionTypes.name AS enrolled, FE.Coverages.annualPremium, FE.Coverages.modalPremium, IFNULL(FE.DependentsToEmployees.relationship, 'EMPLOYEE') AS relationship, FE.Coverages.pending, IFNULL(FE.Coverages.declineReason, 'N/A') AS declineReason
 FROM FE.Coverages, FE.People LEFT JOIN FE.DependentsToEmployees ON FE.People.personId = FE.DependentsToEmployees.dependentId, FE.Offers, FE.Products, FE.CoveredPeople, FE.ElectionTypes
 WHERE FE.ElectionTypes.electionTypeId = FE.Coverages.electionTypeId AND FE.Coverages.coverageId = FE.CoveredPeople.coverageId AND FE.CoveredPeople.personId = FE.People.personId AND FE.Coverages.offerId = FE.Offers.offerId
 AND FE.Offers.productId = FE.Products.productId  ORDER BY FE.People.personId;
