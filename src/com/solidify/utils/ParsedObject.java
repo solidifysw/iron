d0664cb5a46130a811df25a26c06ec37da0d6247
Jonathan Robins <jrobins@solidifysoftware.com>
2015-02-25 18:29:23 -0600

    Updates

--- src/com/solidify/utils/ParsedObject.java
+++ src/com/solidify/utils/ParsedObject.java
@@ -3,9 +3,12 @@ package com.solidify.utils;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Stack;
 
 /**
@@ -13,11 +16,15 @@ import java.util.Stack;
  */
 public class ParsedObject {
     private String json;
-    private ParentObject p;
+    private JSONObject jo;
+    private HashSet<String> skips;
+    private String currentKey;
 
-    public ParsedObject(String json) {
-       this.json = json;
-        this.p = new ParentObject();
+    public ParsedObject(String json, HashSet<String> skips) {
+        this.json = json;
+        this.jo = new JSONObject();
+        this.skips = skips;
+        this.currentKey = "";
         try {
             parse();
         } catch (IOException e) {
@@ -25,42 +32,137 @@ public class ParsedObject {
         }
     }
 
-    private void parse() throws IOException {
+    public JSONObject get() {
+        return jo;
+    }
+
+    public void parse() throws IOException {
         if (json == null || "".equals(json)) {
             return;
         }
         JsonFactory factory = new JsonFactory();
         JsonParser jp = factory.createParser(json);
-        JsonToken current = null;
-        Stack<ParentObject> objects = new Stack<ParentObject>();
-        JSONObject curObj = null;
         String field = null;
 
+        JsonToken current = null;
+        Stack<Object> els = new Stack();
         current = jp.nextToken();
         if (current != JsonToken.START_OBJECT) {
             return;
         }
 
+        els.push(jo);
         while((current = jp.nextToken()) != null) {
-            if (current == JsonToken.FIELD_NAME) {
-                field = jp.getCurrentName();
-            } else if (current == JsonToken.VALUE_STRING) {
-                p.put(field,jp.getValueAsString());
-            } else if (current == JsonToken.START_OBJECT) {
-                objects.push(p);
-                p = new ParentObject(field);
-            } else if (current == JsonToken.END_OBJECT) {
-                if (!objects.isEmpty()) {
-                    ParentObject tmp = objects.pop();
-                    tmp.put(p.getField(),p);
-                    p = tmp;
-                }
+            switch(current) {
+                case FIELD_NAME:
+                    String key = jp.getCurrentName();
+                    System.out.println("currentKey: "+currentKey+" key: "+key);
+                    System.out.println("len: "+currentKey.length());
+                    if (!skips.isEmpty() && currentKey.length() > 0) {
+                        if (skips.contains(currentKey+"."+key)) {
+                            System.out.println("skipping1: " + currentKey + "." + key);
+                            //jp.nextToken();
+                            jp.skipChildren();
+                        } else {
+                            els.push(key);
+                        }
+                    } else if (!skips.isEmpty() && skips.contains(key)) {
+                        System.out.println("skipping2: " + key);
+                        //jp.nextToken();
+                        jp.skipChildren();
+                    } else {
+                        els.push(key);
+                    }
+                    break;
+                case START_OBJECT:
+                    if (els.peek().getClass().equals(String.class)) {
+                        currentKey = "".equals(currentKey) ? (String) els.peek() : currentKey + "." + (String) els.peek();
+                        System.out.println("1: "+currentKey);
+                    }
+                    els.push(new JSONObject());
+                    break;
+                case END_OBJECT:
+                    JSONObject tmp = (JSONObject)els.pop();
+                    if (!els.isEmpty()) {
+                        if (els.peek().getClass().equals(String.class)) {
+                            if (currentKey.length() > 0 && currentKey.contains(".")) {
+                                currentKey = currentKey.substring(0,currentKey.lastIndexOf("."));
+                                System.out.println(currentKey);
+                            } else if (currentKey.length() > 0) {
+                                currentKey = "";
+                            }
+                            field = (String) els.pop();
+                            ((JSONObject) els.peek()).put(field, tmp);
+                        } else if (els.peek().getClass().equals(JSONArray.class)) {
+                            ((JSONArray) els.peek()).put(tmp);
+                        }
+                    }
+                    break;
+                case START_ARRAY:
+                    currentKey = "".equals(currentKey) ? (String) els.peek() : currentKey + "." + (String) els.peek();
+                    System.out.println(currentKey);
+                    els.push(new JSONArray());
+                    break;
+                case END_ARRAY:
+                    if (!els.isEmpty()) {
+                        JSONArray ja = (JSONArray)els.pop();
+                        field = (String)els.pop();
+                        if (currentKey.length() > 0 && currentKey.contains(".")) {
+                            currentKey = currentKey.substring(0,currentKey.lastIndexOf("."));
+                            System.out.println(currentKey);
+                        }
+                        ((JSONObject)els.peek()).put(field,ja);
+                    }
+                    break;
+                case VALUE_STRING:
+                    if (els.peek().getClass().equals(String.class)) {
+                        field = (String) els.pop();
+                        ((JSONObject) els.peek()).put(field, jp.getValueAsString());
+                    } else if (els.peek().getClass().equals(JSONArray.class)) {
+                        ((JSONArray)els.peek()).put(jp.getValueAsString());
+                    }
+                    break;
+                case VALUE_FALSE:
+                    if (els.peek().getClass().equals(String.class)) {
+                        field = (String)els.pop();
+                        ((JSONObject)els.peek()).put(field,false);
+                    } else if (els.peek().getClass().equals(JSONArray.class)) {
+                        ((JSONArray)els.peek()).put(false);
+                    }
+                    break;
+                case VALUE_TRUE:
+                    if (els.peek().getClass().equals(String.class)) {
+                        field = (String)els.pop();
+                        ((JSONObject)els.peek()).put(field, true);
+                    } else if (els.peek().getClass().equals(JSONArray.class)) {
+                        ((JSONArray)els.peek()).put(true);
+                    }
+                    break;
+                case VALUE_NULL:
+                    if (els.peek().getClass().equals(String.class)) {
+                        field = (String)els.pop();
+                        ((JSONObject)els.peek()).put(field, JSONObject.NULL);
+                    } else if (els.peek().getClass().equals(JSONArray.class)) {
+                        ((JSONArray)els.peek()).put(JSONObject.NULL);
+                    }
+                    break;
+                case VALUE_NUMBER_FLOAT:
+                    if (els.peek().getClass().equals(String.class)) {
+                        field = (String)els.pop();
+                        ((JSONObject)els.peek()).put(field, jp.getFloatValue());
+                    } else if (els.peek().getClass().equals(JSONArray.class)) {
+                        ((JSONArray)els.peek()).put(jp.getFloatValue());
+                    }
+                    break;
+                case VALUE_NUMBER_INT:
+                    if (els.peek().getClass().equals(String.class)) {
+                        field = (String)els.pop();
+                        ((JSONObject)els.peek()).put(field, jp.getLongValue());
+                    } else if (els.peek().getClass().equals(JSONArray.class)) {
+                        ((JSONArray)els.peek()).put(jp.getLongValue());
+                    }
+                    break;
             }
         }
-        //System.out.println(p.toString());
-    }
-
-    public JSONObject get() {
-        return p;
     }
 }
--- tests/com/solidify/tests/TestParsedObject.java
+++ tests/com/solidify/tests/TestParsedObject.java
@@ -1,8 +1,11 @@
 package com.solidify.tests;
 
 import com.solidify.utils.ParsedObject;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
+
+import java.util.HashSet;
 
 import static org.junit.Assert.assertTrue;
 
@@ -12,18 +15,73 @@ import static org.junit.Assert.assertTrue;
 public class TestParsedObject {
     @Test
     public void testParsedObject() {
-        JSONObject jo = new JSONObject();
-        jo.put("a","1");
-        jo.put("b","2");
+        long num = 123456789123456789l;
+
+       /* JSONObject jo = new JSONObject();
+        jo.put("a",1);
+        jo.put("b",2.35);
+        jo.put("ab",true);
+        jo.put("de",false);
+        jo.put("ge",num);
         JSONObject jo2 = new JSONObject();
         jo2.put("c","3");
         JSONObject jo3 = new JSONObject();
         jo3.put("d","4");
         jo2.put("y",jo3);
         jo.put("x",jo2);
+        JSONArray ja = new JSONArray();
+        ja.put("entry1");
+        ja.put("entry2");
+        jo.put("array",ja);
         ParsedObject po = new ParsedObject(jo.toString());
         JSONObject obj = po.get();
-        System.out.println(obj.toString());
-        assertTrue(obj.has("a"));
+        */
+        JSONObject obj = new JSONObject();
+        JSONObject emptyObject = new JSONObject();
+        obj.put("member",emptyObject);
+        obj.put("declineReasons",emptyObject);
+        obj.put("attended",true);
+        obj.put("classId","12345");
+        obj.put("disclosureQuestions", emptyObject);
+        obj.put("id","abcdef");
+
+        JSONObject data = new JSONObject();
+        JSONObject member = new JSONObject();
+        member.put("id","abc123");
+        JSONObject personal = new JSONObject();
+        personal.put("firstName","John");
+        member.put("personal",personal);
+        data.put("member",member);
+
+        JSONObject cov = new JSONObject();
+        cov.put("planName","test");
+        data.put("0ee34f3e=05ac",cov);
+
+        JSONArray array = new JSONArray();
+        JSONObject sp = new JSONObject();
+        sp.put("firstName","Jane");
+        sp.put("lastName","Doe");
+        sp.put("relationship","SPOUSE");
+        sp.put("id","123");
+        sp.put("zip","");
+        sp.put("address1","123 Elm St.");
+        sp.put("city","Fairfax");
+        sp.put("state", "VA");
+
+        array.put(sp);
+        data.put("beneficiaries",array);
+        data.put("dependents",array);
+
+        obj.put("data",data);
+        obj.put("enrollment",emptyObject);
+
+        HashSet<String> skips = new HashSet();
+        skips.add("data.member.personal");
+        skips.add("enrollment");
+        skips.add("member");
+        ParsedObject po = new ParsedObject(obj.toString(), skips);
+        JSONObject testObj = po.get();
+        System.out.println(testObj.toString());
+        assertTrue(obj.has("member"));
     }
 }
